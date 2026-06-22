07d3c43779125b5479f005f37886202e67dc5bed
Ayushman <ayushmantripathi283@gmail.com>
2026-06-22 10:57:59 +0530

    added dummy portfolio

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,19 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+
+import ContactHero from "@/components/contact/ContactHero";
+import ContactSection from "@/components/contact/ContactSection";
+
+export default function ContactPage() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>porfolio</h1>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
